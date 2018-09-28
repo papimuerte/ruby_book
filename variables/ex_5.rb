4bc1b8a50bb8b7a9dfe2e0b55621f5d749fa1947
Nathan Ward <Nate@Nathans-MacBook-Pro.local>
2018-09-27 20:03:33 -0600

    Chapter 2

---
+++ variables/ex_5.rb
@@ -0,0 +1,2 @@
+#In ths first program x = 3 and does not show any errors as x is defined prior to the do/end
+#In the second program there is an undefined error as x is not defined prior to the do/end so the put x is outputing an undefined variable.
