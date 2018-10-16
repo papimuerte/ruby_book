77d338b067e35c325049ed4cecbe159d7bc83726
Papimuerte <wakensnow>
2018-10-15 20:48:57 -0600

    Chapter 6

---
+++ arrays/ex6.rb
@@ -0,0 +1,4 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+
+# As you are trying to change an item in the array but instead of using the integer index you are entering in a string. It should be a integer for "margaret" which should be [3]
---
+++ arrays/ex7.rb
@@ -0,0 +1,10 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+arr2 = arr.map do |x| x + 2
+
+	end
+
+
+	p arr
+	p arr2 
